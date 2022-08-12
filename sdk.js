cd8984bcb2cd7d0c8c1ceb5680e70500668f2329
yisen <yisen@gaoding.com>
2022-08-12 10:41:32 +0800

    feat: 全局变量注入

--- sdk.js
+++ sdk.js
@@ -1,6 +1,6 @@
 function setTpl(tpl) {
     const setTemplet = document.querySelector('.editor-container').__vue__.setTemplet;
-    
+
     if (typeof tpl !== 'string') {
         return setTemplet(tpl)
     }
@@ -11,7 +11,32 @@ function setTpl(tpl) {
         });
 }
 
+function getEditorCurrentInfo(global) {
+    const editor = document.querySelector('.editor-container').__vue__;
+    const scope = document.querySelector('.editor-container').__vue__.$parent.$parent.$parent.$parent.$parent.$parent.$parent;
+
+    const {
+        currentElement,
+        currentSubElement,
+        currentLayout
+    } = editor;
+    const current = {
+        scope,
+        editor,
+        currentElement,
+        currentSubElement,
+        currentLayout,
+        tpl: scope.templet,
+    }
+
+    Object.assign(global, current);
+    global.copy(scope.templet);
+    
+    console.log('current ====> ', current);
+}
+
 // 注入 window
 Object.assign(window, {
-    setTpl
+    setTpl,
+    crt: getEditorCurrentInfo
 })
